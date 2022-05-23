4fc954e1f3e4934d86ac5b6b688d6f92e938244b
mustardjelly <32181718+mustardjelly@users.noreply.github.com>
2022-05-23 13:19:04 +1200

    Create guess_number_game.py

---
+++ guess_number_game.py
@@ -0,0 +1,18 @@
+# Create some code to randomly select a number under 10
+# The user must try to guess this number in 3 or less guesses
+# If the users guess is too low, tell them
+# If the users guess is too high, tell them
+# There is a strategy to this game which will allow you to always win. Figure out that strategy
+
+/* Example
+computer chooses 3
+3 guesses remaining
+user guesses 1
+"too low"
+2 guesses remaining
+user guesses 9
+"too high"
+1 guess remaining
+user guesses 3
+"Correct"
+*/
