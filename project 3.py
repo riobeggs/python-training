eda80e6c62ad40d56464038a4c0ae5906f81c176
Rio Beggs <riobeggs@Rios-MacBook-Air.local>
2022-05-24 15:47:20 +1200

    changed guesses from 10 to 20

--- project 3.py
+++ project 3.py
@@ -2,7 +2,7 @@ import random
 
 #introduces and starts the game
 def intro():
-    print("\nGuess the 4 letter word in 10 attempts to win!\n")
+    print("\nGuess the 4 letter word in 20 attempts to win!\n")
     input("Press Enter to start")
     print("\n")
 
@@ -16,13 +16,13 @@ def chosen_word():
 #if they guess correctly within 10 guesses they win.
 #if they guess incorrectly or run out of guesses they lose.
 def users_guess(word):
-    for i in range(10):
-        if 10-i==1:
+    for i in range(20):
+        if 20-i==1:
             print("This is your last guess!")
             answer(word)
             break
         else:
-            print("You have", 10-i, "guesses left.")
+            print("You have", 20-i, "guesses left.")
         guess = input("Guess: ").lower()
         if len(guess)!=4:
             print("Incorrect\nYou must guess 4 letter words only.\n")
