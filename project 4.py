3847823e2c391194fe66ead840374db054a7e3b7
Rio Beggs <riobeggs@Rios-MacBook-Air.local>
2022-05-29 15:30:39 +1200

    just need to add in ability to exit calculator when user inputs 'exit'

--- project 4.py
+++ project 4.py
@@ -1,71 +1,75 @@
-#calculator
-#tell user how to use the calculator
-# + = add , x = multiply , - = minus , / = divide.
-#print the answer to their input
-
-
-#ask for the equation
-#if their inpout is exit then exit the program
-#if their input is not an integer then ask for equation
-#if their input does not take * / + - = then ask for equation
-
-
-
-#intro
 def instructions():
     print("\n\nYou may use this calculator as many times as you like.")
     print("To exit the calculator, type 'exit', then press enter.")
     print("\nAdd using the '+' key.")
     print("Minus using the '-' key.")
     print("Multiply using the 'x' key.")
-    print("Divide using the '/' key.\n\n")
+    print("Divide using the '/' key.\n")
 
 
-def convert_to_int(equation):
+def convert_to_float(equation):
   converted = None
   try:
-      converted = int(equation)
+      converted = float(equation)
   except: 
       pass
 
   return converted
 
-def first_number():
+
+def get_first_number():
     while True:
         num1 = input("First number: ")
-        converted_num1 = convert_to_int(num1)
+        converted_num1 = convert_to_float(num1)
 
         if converted_num1 == None:
             continue
 
         return converted_num1
 
-#multiplication function
 
-#enter a first number
-#enter an operator
-#enter a 2nd number
-
-
-# gets users input/ equation
-def get_equation():
+def get_operator():
     while True:
-        equation = input("Equation: ")
-        if equation == "exit":
-            break
+        available_operators = ["+" , "-" , "x" , "/"]
+        operator = input("Operator: ")
 
-        converted_equation = convert_to_int(equation)
-        if converted_equation == None:
+        if operator not in available_operators:
             continue
-        
-        if converted_equation == int(converted_equation):
-            return converted_equation
-        
-        
 
-instructions()
-converted_equation = get_equation()
-print(converted_equation)
-    
+        return operator
 
 
+def get_second_number():
+    while True:
+        num2 = input("Second number: ")
+        print("\n")
+        converted_num2 = convert_to_float(num2)
+
+        if converted_num2 == None:
+            continue
+
+        return converted_num2
+
+
+def calculate_answer(num1, operator, num2):
+    if operator == "+":
+        print(num1, "+", num2, "=", num1 + num2, "\n\n")
+
+    if operator == "-":
+        print(num1, "-", num2, "=", num1 - num2, "\n\n")
+
+    if operator == "x":
+        print(num1, "x", num2, "=", num1 * num2, "\n\n")
+
+    if operator == "/":
+        print(num1, "/", num2, "=", num1 / num2, "\n\n")       
+
+def run_calculator():
+    instructions()
+    num1 = get_first_number()
+    operator = get_operator()
+    num2 = get_second_number()
+    calculate_answer(num1, operator, num2)
+
+
+run_calculator()
