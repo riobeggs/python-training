4981f057a7b97e53f18291699bb3f22ec2bd0279
Rio Beggs <riobeggs@Rios-MacBook-Air.local>
2022-05-29 14:31:12 +1200

    problems with get_equation. Have to get inputs for each number and operator

--- project 4.py
+++ project 4.py
@@ -4,6 +4,13 @@
 #print the answer to their input
 
 
+#ask for the equation
+#if their inpout is exit then exit the program
+#if their input is not an integer then ask for equation
+#if their input does not take * / + - = then ask for equation
+
+
+
 #intro
 def instructions():
     print("\n\nYou may use this calculator as many times as you like.")
@@ -12,5 +19,53 @@ def instructions():
     print("Minus using the '-' key.")
     print("Multiply using the 'x' key.")
     print("Divide using the '/' key.\n\n")
+
+
+def convert_to_int(equation):
+  converted = None
+  try:
+      converted = int(equation)
+  except: 
+      pass
+
+  return converted
+
+def first_number():
+    while True:
+        num1 = input("First number: ")
+        converted_num1 = convert_to_int(num1)
+
+        if converted_num1 == None:
+            continue
+
+        return converted_num1
+
+#multiplication function
+
+#enter a first number
+#enter an operator
+#enter a 2nd number
+
+
+# gets users input/ equation
+def get_equation():
+    while True:
+        equation = input("Equation: ")
+        if equation == "exit":
+            break
+
+        converted_equation = convert_to_int(equation)
+        if converted_equation == None:
+            continue
+        
+        if converted_equation == int(converted_equation):
+            return converted_equation
+        
+        
+
+instructions()
+converted_equation = get_equation()
+print(converted_equation)
+    
 
 
