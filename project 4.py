ab883c952a9a628c3a60d2bfe171b6d6c05c825a
Rio Beggs <riobeggs@Rios-MacBook-Air.local>
2022-05-29 13:23:34 +1200

    created intro

---
+++ project 4.py
@@ -0,0 +1,16 @@
+#calculator
+#tell user how to use the calculator
+# + = add , x = multiply , - = minus , / = divide.
+#print the answer to their input
+
+
+#intro
+def instructions():
+    print("\n\nYou may use this calculator as many times as you like.")
+    print("To exit the calculator, type 'exit', then press enter.")
+    print("\nAdd using the '+' key.")
+    print("Minus using the '-' key.")
+    print("Multiply using the 'x' key.")
+    print("Divide using the '/' key.\n\n")
+
+
