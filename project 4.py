a9ff0523d6ad6ca3266ac0eec6cb2af7c66041cf
Rio Beggs <riobeggs@Rios-MacBook-Air.local>
2022-05-30 02:06:31 +1200

    finished calculator

--- project 4.py
+++ project 4.py
@@ -1,8 +1,9 @@
 import sys
 import operator
 
-
+# function for storing properties of operators in a dictionary 
 def operator_dict():
+    # also associates operators with the users input and variables
     ops = {"+" : operator.add,
         "-" : operator.sub,
         "x" : operator.mul,
@@ -10,83 +11,106 @@ def operator_dict():
     return ops
 
 
+# gives the user instructions on how to use the calculator
 def instructions():
     print("\n\nYou may use this calculator as many times as you like.")
+    # shows how to exit program
     print("To exit the calculator, type 'quit', then press enter.")
+    # gives the user the operators available to use
     print("\nAdd using the '+' key.")
     print("Subtract using the '-' key.")
     print("Multiply using the 'x' key.")
     print("Divide using the '/' key.\n")
 
 
+# trys to convert the users input to a float
 def convert_to_int(equation):
-  converted = None
-  try:
-      converted = float(equation)
-  except: 
-      pass
+    converted = None
+    # if the input cant be converted to a float then it returns as None
+    try:
+        converted = float(equation)
+        # retuns as float if it can be converted to a float
+    except: 
+        pass
 
-  return converted
+    return converted
 
 
+# gets the users input for the first number in the equation
 def get_first_number():
     while True:
         num1 = input("First number: ")
 
+        # quits program if they enter 'quit'
         if num1 == "quit":
             sys.exit(0)
 
         converted_num1 = convert_to_int(num1)
 
+        # if the users input is not a float start loop again (ask for input)
         if converted_num1 == None:
             continue
 
         return converted_num1
 
 
+# gets the users input for the operator that want to use
 def get_operator():
     while True:
         operator = input("Operator: ")
 
+        # quits program if they enter 'quit'
         if operator == "quit":
             sys.exit(0)
 
         ops = operator_dict()
+        
+        # if the chosen operator is not a real operator
+        # and is not in the available operators created in the operator dictionary
+        # then ask for the users input again
         if operator not in ops:
             continue
 
         return operator
 
 
+# gets the users input for the second number in the equation
 def get_second_number():
     while True:
         num2 = input("Second number: ")
 
+        # quits program if they enter 'quit'
         if num2 == "quit":
             sys.exit(0)
 
         print("\n")
 
         converted_num2 = convert_to_int(num2)
-
+        
+        # if the users input is not a float start loop again (ask for input)
         if converted_num2 == None:
             continue
 
         return converted_num2
 
 
+# prints and calculates the answer to the users equation
 def calculate_answer(num1, operator, num2):
     ops = operator_dict()
     print(num1, operator, num2, "=", ops[operator](num1,num2), "\n\n")      
 
 
+# calls functions that compile the program
 def run_calculator():
+    # excecutes instructions function
     instructions()
     while True:
+        # forever repeats the calculator until the user enters 'exit'
         num1 = get_first_number()
         operator = get_operator()
         num2 = get_second_number()
         calculate_answer(num1, operator, num2)
 
 
+# excecutes the program
 run_calculator()
