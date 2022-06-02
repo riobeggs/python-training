aecaaeffa9a79349aca595dc40b3e7d9c1adcaad
mustardjelly <32181718+mustardjelly@users.noreply.github.com>
2022-06-02 20:54:11 +1200

    Refactor

--- calculator_class.py
+++ calculator_class.py
@@ -1,7 +1,8 @@
 import operator
+from typing import Any, Union
 
 
-#constants
+# constants
 available_operators = ["+", "-", "x", "*", "/"]
 OPERATIONS = {
     "+": operator.add,
@@ -12,53 +13,86 @@ OPERATIONS = {
 }
 
 
+def instructions():
+    """Prints the instructions for usage."""
+    print("\n\nYou may use this calculator as many times as you like.")
+    print("To exit the calculator, type 'quit', then press enter.")
+    print()
+    print("Add using the '+' key.")
+    print("Subtract using the '-' key.")
+    print("Multiply using the 'x' key.")
+    print("Divide using the '/' key.")
+    print()
+
+
 class Calculator:
-    def __init__(self, num1, operator, num2):
-        self.num1 = num1
-        self.operator = operator
-        self.num2 = num2
+    """A calculator capable of :gasp: calculating."""
 
+    def calculate(self, equation: "Equation") -> Union[float, None]:
+        """Calculate a result from an equation."""
+        if equation.can_calculate():
+            return OPERATIONS[equation.operator](
+                equation.number_one, equation.number_two
+            )
 
-    def instructions():
-        print("\n\nYou may use this calculator as many times as you like.")
-        print("To exit the calculator, type 'quit', then press enter.")
-        print()
-        print("Add using the '+' key.")
-        print("Subtract using the '-' key.")
-        print("Multiply using the 'x' key.")
-        print("Divide using the '/' key.")
-        print()
+    def print_result(self, equation: "Equation") -> None:
+        """Prints the result if one exists."""
 
+        result = self.calculate(equation)
+        if result:
+            print()
+            print(
+                equation.number_one, equation.operator, equation.number_two, "=", result
+            )
+            print()
 
-    def calculate_answer(self):
-        print()
-        print(self.num1, self.operator, self.num2, "=", OPERATIONS[self.operator](self.num1, self.num2))
-        print()
-   
+            return
+
+        print("Error: Equation has no result.")
+
 
 class Equation:
-    def __init__(self, question_prefix, number, converted_number, operator):
-        self.question_prefix = question_prefix
-        self.number = number
-        self.converted_number = converted_number
-        self.operator = operator
+    """An equation object holding details for and calculating."""
 
+    def __init__(self):
+        self.number_one: float = None
+        self.number_two: float = None
+        self.operator: operator = None
+        self.result: float = None
 
-    def get_number(self):
+    def set_number_one(self) -> "Equation":
+        """Sets number one of our equation.
+        
+        Self is a fun thing to return."""
         while True:
-            self.number = input(f"{self.question_prefix} number: ")
-            self.converted_number = self._convert_to_float(self.number)
+            number_one = input("First number: ")
+            self.number_one = self._convert_to_float(number_one)
 
-            if self.converted_number == None:
+            if not self.number_one:
                 continue
 
-            return self.converted_number
+            return self
 
-            
-    def _convert_to_float(number):
+    def set_number_two(self) -> "Equation":
+        """Sets number two of our equation.
+        
+        Self is a fun thing to return."""
+        while True:
+            number_two = input("Second number: ")
+            self.number_two = self._convert_to_float(number_two)
+
+            if not self.number_two:
+                continue
+
+            return self
+
+    def _convert_to_float(self, number: Any) -> Union[float, None]:
         """As this class is never called externally, we prefix it with an "_". 
         
         Attempts to convert a number to a float.
+
+        Returns None if no conversion. 
+        Returns float if conversion is successful.
         """
         converted = None
         try:
@@ -68,35 +102,68 @@ class Equation:
 
         return converted
 
-
-    def get_operator(self):
+    def set_operator(self) -> "Equation":
+        """Sets the operator of our equation."""
         while True:
             self.operator = input("Operator: ")
             if self.operator not in OPERATIONS:
                 continue
-            return self.operator
+
+            return self
+
+    def _is_valid(self) -> bool:
+        """
+        We underscore the start when we only intend a function to be used internally.
+
+        Reports if the equation is valid and calculate can be used.
+        """
+        return self.number_one and self.number_two and self.operator
+
+    def can_calculate(self) -> bool:
+        """Determines if a calculation can be made."""
+        if not self._is_valid():
+            missing_attriute = []
+            attributes = [self.number_one, self.number_two, self.operator, self.result]
+            for attribute in attributes:
+                if not attribute:
+                    missing_attriute.append(attribute)
+
+            print(f"ERROR: The following attributes were missing: {missing_attriute}")
+            return False
+
+        return True
+
+    def reset(self) -> None:
+        """Reset the equation."""
+        self.__init__()
 
 
+def main():
+    instructions()
 
-Calculator.instructions()
-num1 = Equation(question_prefix="First", number=None, converted_number=None, operator=None).get_number()
-operator = Equation(question_prefix=None, number=None, converted_number=None, operator=None).get_operator()
-num2 = Equation(question_prefix="Second", number=None, converted_number=None, operator=None).get_number()
-calculator = Calculator(num1=num1, operator=operator, num2=num2) 
-calculator.calculate_answer()
+    equation = Equation()
+    calculator = Calculator()
+
+    while True:
+        equation.set_number_one().set_number_two().set_operator()
+        calculator.print_result(equation)
+
+        equation.reset()
 
 
-    # Define a function which gives instruction how to use the calculator
+if __name__ == "__main__":
+    main()
 
-    # Create multiply func
-    # Create devide func
-    # Create add func
-    # Create subtract func
+# Define a function which gives instruction how to use the calculator
 
+# Create multiply func
+# Create devide func
+# Create add func
+# Create subtract func
 
 
 # Once you have created a calculator class capable of performing calculations
-# Create project 4 again, but this time use the calculator. 
+# Create project 4 again, but this time use the calculator.
 # You must use my_calculator.Add(5, 6) for adding for instance
 
 # I really dont expect you to be able to do this.
