11d259248c304f7f8b897800bbf3902ba1cf4b06
Rio Beggs <riobeggs@Rios-MacBook-Air.local>
2022-06-10 11:55:07 +1200

    created file

---
+++ advent_day3.py
@@ -0,0 +1,11 @@
+import random
+
+step_north = 0
+step_south = 0
+step_east = 0
+step_west = 0
+
+#delivers present at starting location
+# loop
+    #randomiser chooses from either north, south, east or west.
+    #whatever the randomise chooses,
