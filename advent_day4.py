81cf6375df0c3a453c4fc9ede002581f9193f9b0
Rio Beggs <riobeggs@Rios-MacBook-Air.local>
2022-06-15 12:08:07 +1200

    finished day4 part 1

--- advent_day4.py
+++ advent_day4.py
@@ -1,12 +1,23 @@
 import hashlib
 
 
-key = "yzbqklnj"
+# part 1
+def hash(answer : int) -> str:
+    while True:
+        key = "yzbqklnj" 
+        key = (key + str(answer))
+        encoded_key = hashlib.md5(key.encode())
+        hashed_key = (encoded_key.hexdigest())
+
+        if hashed_key[:5] == "00000":
+            return answer
+
+        answer += 1
+    
+    
+def main() -> None:
+    print(hash(0))
 
 
-def hash_input():
-    hexidemical_hash = hashlib.md5(key.encode())
-    return (hexidemical_hash.hexdigest())
-
-
-print(hash_input())
+if __name__ == "__main__":
+    main()
