2b604373860bb7c411ced7b7952d2fc9cacc20d6
Rio Beggs <riobeggs@Rios-MacBook-Air.local>
2022-06-17 21:41:08 +1200

    finished day 6 part 1

--- advent_day6.py
+++ advent_day6.py
@@ -5,8 +5,10 @@ def grid_base() -> list:
     grid = []
 
     for x in range(1000):
+        row = []
         for y in range(1000):
-            grid.append(0)
+            row.append(0)
+        grid.extend([row])
 
     return grid
 
@@ -14,58 +16,54 @@ def grid_base() -> list:
 def input_list() -> list:
     input_ = open("/Users/riobeggs/Downloads/day6.txt", "r")
     input_ = input_.read().split("\n")
-    input_ = ["turn on 0,0 through 999,999", "turn off 499,499 through 500,500"]
     return input_
 
 
-def int_coordinate_list(instructions : list, grid : list) -> None:
+def find_coordinates(instructions : list) -> list:
     x1, y1, x2, y2 = instructions[0], instructions[1], instructions[2], instructions[3]
     x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+    coordinates = [x1, y1, x2, y2]
+    return coordinates
 
-    # grid[x1 : x2] == 1
-
-    # coordinates = [x1, y1, x2, y2]
-    # return coordinates
-
-
-def lights_on(input_ : list) -> str:
+def lights_on(input_ : list) -> int:
     lights_on = 0
-
     grid = grid_base()
 
     for instructions in input_:
         if "turn on" in instructions:
             instructions = instructions.replace("turn on ", "").replace(" through ", ",")
             instructions = instructions.split(",")
-
-            x1, y1, x2, y2 = instructions[0], instructions[1], instructions[2], instructions[3]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-
-            row = grid[x1 : x2 + 1] = [1] * ((x2 - x1 + 1))
-            print(len(row))
-            # for i in range(len(grid)):
-            #     if i >= x1:
-            #         if i >= x2 + 1:
-            #             continue
-            #         grid[i] = 1
+            coordinates = find_coordinates(instructions)
+            x1, y1, x2, y2 = coordinates[0], coordinates[1], coordinates[2], coordinates[3]
+            for x in range(x1, x2 + 1):
+                for y in range(y1, y2 + 1):
+                    grid[x][y] = 1
+            continue
             
-            # print(grid)
-            #     grid[i] == 1
-            # grid == 1
-            # print(grid[x1 : x2 + 1])
-            # coordinates = int_coordinate_list(instructions, grid)
-            # continue
-            
-        # if "turn off" in instructions:
-        #     instructions = instructions.replace("turn off ", "").replace(" through ", ",")
-        #     instructions = instructions.split(",")
-        #     coordinates = int_coordinate_list(instructions, grid)
-        #     continue
+        if "turn off" in instructions:
+            instructions = instructions.replace("turn off ", "").replace(" through ", ",")
+            instructions = instructions.split(",")
+            coordinates = find_coordinates(instructions)
+            x1, y1, x2, y2 = coordinates[0], coordinates[1], coordinates[2], coordinates[3]
+            for x in range(x1, x2 + 1):
+                for y in range(y1, y2 + 1):
+                    grid[x][y] = 0
+            continue
 
-        # if "toggle" in instructions:
-        #     instructions = instructions.replace("toggle ", "").replace(" through ", ",")
-        #     coordinates = instructions.split(",")
-        
+        if "toggle" in instructions:
+            instructions = instructions.replace("toggle ", "").replace(" through ", ",")
+            instructions = instructions.split(",")
+            coordinates = find_coordinates(instructions)
+            x1, y1, x2, y2 = coordinates[0], coordinates[1], coordinates[2], coordinates[3]            
+            for x in range(x1, x2 + 1):
+                for y in range(y1, y2 + 1):
+                    if grid[x][y] == 0:
+                        grid[x][y] = 1
+                    else:
+                        grid[x][y] = 0
+
+    for row in grid:
+        lights_on += sum(row)                    
 
     return lights_on
 
